85455a42477cfdf366a30dcea12f60353388e18f
Zachary McNulty <zacharymcnulty123@outlook.com>
2018-11-09 20:29:16 -0500

    Updated Folder Name

--- hello/old_macdonald.py
+++
@@ -1,6 +0,0 @@
-#OLD MACDONALD write a function that capitalizes the first and fourth letters of a name
-def old_macdonald(text):
-    text = text[0].upper() + text[1:3] + text[3].upper() + text[4:]
-    return text
-
-print(old_macdonald('macdonald'))
--- hello/print_big.py
+++
@@ -1,8 +0,0 @@
-# PRINT BIG: Write a function that takes in a single letter, and returns a 5 x 5 representation of that letter
-myDict = {
-    "a": "   *\n  * *\n *****\n *   *\n *   *"
-}
-
-def print_big(letter):
-    return myDict[letter.lower()]
-print(print_big('a'))
